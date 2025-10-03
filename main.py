cc8a752efee6da48be354e535dd1afb849c33e37
Mayaramfc <mayaramfc12@gmail.com>
2025-10-03 19:40:04 -0300

    colocando as rotas

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-
-from flask import Flask 
+from flask import Flask, render_template
 app = Flask(__name__)
-from controllers.usuario_controller import *
-if __name__ == "__main__":
-  app.run()
+
+#rota para página inicial (login)
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+#rota para o menu principal
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+#rota para cadastro de veículos
+
+@app.route('/cadastrar-veiculos')
+def cadastrar_veículos():
+    return render_template('Tela-de-cadastrar-veículo.html')
